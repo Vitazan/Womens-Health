bb11edc2fa31c063cbbac3ad6d4f7b398059c338
Nicolas Desjardins <nick.desjardins@live.com>
2025-07-29 08:36:17 -0400

    new unbalanced height for logos....

--- components/header.tsx
+++ components/header.tsx
@@ -35,7 +35,7 @@ const Header = () => {
             <motion.img
               src="/nfh.png"
               alt="NFH Logo"
-              className="h-16 p-3"
+              className="h-[76px] p-3"
               initial={{ opacity: 0, y: -10 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5 }}
